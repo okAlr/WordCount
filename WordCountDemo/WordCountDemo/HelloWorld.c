c7f52007e8996d04801e69d38a16fe43e307e186
abby <3499954064@qq.com>
2021-05-07 18:30:10 +0800

    update

---
+++ WordCountDemo/WordCountDemo/HelloWorld.c
@@ -0,0 +1,8 @@
+//
+//  HelloWorld.c
+//  WordCountDemo
+//
+//  Created by Abby on 2021/5/7.
+//
+
+#include "HelloWorld.h"
---
+++ WordCountDemo/WordCountDemo/HelloWorld.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorld.h
+//  WordCountDemo
+//
+//  Created by Abby on 2021/5/7.
+//
+
+#ifndef HelloWorld_h
+#define HelloWorld_h
+
+#include <stdio.h>
+
+#endif /* HelloWorld_h */
